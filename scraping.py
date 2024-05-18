6ccac8fdaec6c502f16ef0a7ba5977b2a33b30c1
ericbuys <eric.buys02@gmail.com>
2024-05-18 19:13:23 -0400

    Finished non-connecting scraping

--- scraping.py
+++ scraping.py
@@ -2,6 +2,53 @@ import requests
 import os
 import re
 from datetime import datetime
+import base64
+
+FILE_TYPES = {
+    "C": (".c", ".h"),
+    "C++": (".cpp", ".hpp", ".cc", ".cxx", ".h", ".hh", ".hxx"),
+    "C#": (".cs"),
+    "Java": (".java"),
+    "Python": (".py"),
+    "JavaScript": (".js"),
+    "TypeScript": (".ts"),
+    "Ruby": (".rb"),
+    "Go": (".go"),
+    "Rust": (".rs"),
+    "Swift": (".swift"),
+    "Kotlin": (".kt", ".kts"),
+    "HTML": (".html", ".htm"),
+    "CSS": (".css"),
+    "PHP": (".php"),
+    "ASP.NET": (".aspx", ".ascx", ".ashx", ".asmx"),
+    "Bash": (".sh"),
+    "Perl": (".pl", ".pm"),
+    "PowerShell": (".ps1"),
+    "Batch": (".bat", ".cmd"),
+    "JSON": (".json"),
+    "XML": (".xml"),
+    "YAML": (".yaml", ".yml"),
+    "CSV": (".csv"),
+    "Markdown": (".md"),
+    "SCSS": (".scss"),
+    "Sass": (".sass"),
+    "Less": (".less"),
+    "SQL": (".sql"),
+    "CMake": (".cmake"),
+    "Haskell": (".hs"),
+    "Erlang": (".erl", ".hrl"),
+    "Lisp": (".lisp", ".lsp"),
+    "Scheme": (".scm", ".ss"),
+    "Prolog": (".pl", ".pro"),
+    "R": (".R", ".r"),
+    "MATLAB": (".m"),
+    "Julia": (".jl"),
+    "SAS": (".sas"),
+    "Assembly": (".asm", ".s"),
+    "Fortran": (".f", ".for", ".f90", ".f95"),
+    "COBOL": (".cob", ".cbl"),
+    "Ada": (".adb", ".ads"),
+}
 
 def extract_github_username(url):
     pattern = r"github\.com/([A-Za-z0-9-]+)"
@@ -75,16 +122,27 @@ def calculate_repo_popularity(repo):
 def parse_github_time_str(date_str):
     return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
 
-def filter_repos_by_languages(username, token, repo_list, languages):
+def filter_repos_by_languages(username, token, repo_list, languages, limit=None):
     repos_by_language = {}
 
     if type(languages) == str:
         repos_by_language[languages] = []
+    else:
+        for language in languages:
+            repos_by_language[language] = []
 
     print(f"{repo_list=}")
     for repo in repo_list:
         repo_languages = get_repo_languages(username, token, repo['name'])
-        print(repo['name'], repo_languages)
+        
+        for language in repo_languages:
+            try:
+                if limit == None or len(repos_by_language[language]) < limit:
+                    repos_by_language[language].append(repo['name'])
+            except:
+                pass
+    
+    return repos_by_language
 
 def get_repo_languages(username, token, repo):
     url = f"https://api.github.com/repos/{username}/{repo}/languages"
@@ -100,8 +158,22 @@ def get_repo_languages(username, token, repo):
         print(f"Failed to retrieve languages: {response.status_code}")
         return None
 
-def list_files(repo_owner, repo_name, token, path=""):
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{path}"
+def get_files_to_scrape(username, token, language_repo_dict):
+    files = {}
+
+    for language, repo_list in language_repo_dict.items():
+        try:
+            for repo in repo_list:
+                lang_files = []
+                locate_files(username, token, repo, language, lang_files)
+                files[language] = {"repo": repo, "files": lang_files}
+        except:
+            pass
+
+    return files
+
+def locate_files(username, token, repo_name, language, files, path=""):
+    url = f"https://api.github.com/repos/{username}/{repo_name}/contents/{path}"
     headers = {
         "Authorization": f"token {token}",
         "Accept": "application/vnd.github.v3+json"
@@ -111,12 +183,41 @@ def list_files(repo_owner, repo_name, token, path=""):
     if response.status_code == 200:
         contents = response.json()
         for item in contents:
-            if item['type'] == 'file':
-                print(item['path'])
+            if item['type'] == 'file' and item['path'].endswith(FILE_TYPES[language]):
+                files.append(item['path'])
             elif item['type'] == 'dir':
-                list_files(repo_owner, repo_name, token, item['path'])
+                locate_files(username, token, repo_name, language, files, item['path'])
     else:
         print(f"Failed to retrieve contents: {response.status_code}")
+
+def retrieve_files(username, token, repo_files_dict):
+    raw_file_content = {}
+
+    for language, location in repo_files_dict.items():
+        repo = location["repo"]
+        raw_file_content[language] = {"repo": repo, "files": []}
+        
+        for path in location['files']:
+            raw_code = retrieve_file_from_repo(username, token, repo, path)
+            raw_file_content[language]["files"].append({"name": path, "content": raw_code})
+    
+    return raw_file_content
+
+def retrieve_file_from_repo(username, token, repo, path):
+    url = f"https://api.github.com/repos/{username}/{repo}/contents/{path}"
+    headers = {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        content = response.json().get("content")
+        decoded_content = base64.b64decode(content).decode("utf-8")
+        return decoded_content
+    else:
+        print(f"Failed to retrieve file. Status code: {response.status_code}")
+        return None
 
 if __name__ == "__main__":
     token = os.getenv("GITHUB_TOKEN")
@@ -125,23 +226,20 @@ if __name__ == "__main__":
         exit
 
     # Get username
-    github_profile_url = "https://github.com/Isss11"
+    github_profile_url = "https://github.com/ericbuys"
+    # github_profile_url = "https://github.com/Isss11"
     # github_profile_url = "https://github.com/joelharder4?tab=repositories"
     # github_profile_url = "https://github.com/wiwichips?page=1&tab=repositories"
     username = extract_github_username(github_profile_url)
-    languages = ["Java", "Python"]
+    languages = ["Python", "HTML"]
 
-
+    # Get repos
     repos = get_repo_list(username, token)
-    repos = filter_repos_by_languages(username, token, repos, languages)
-    print(len(repos), repos)
-    #     # 
-    #     print(get_repo_languages(username, repo_name, token))
-    
-    # list_files(username, repo_name, token)
+    language_repo_dict = filter_repos_by_languages(username, token, repos, languages, limit=1)
+    print(f"{language_repo_dict=}")
 
-    # if token is None:
-    #     print("Error: GITHUB_TOKEN environment variable not set")
-    # else:
-    #     # list_repos(username, token)
-    #     print(get_repo_languages(username, repo_name, token))
+    # Get file content
+    files = get_files_to_scrape(username, token, language_repo_dict)
+    print(f"{files=}")
+    file_content = retrieve_files(username, token, files)
+    print(f"{file_content=}")
