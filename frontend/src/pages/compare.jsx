f61e74b029750142218c0ec90aa79b3d0b6dca9e
Isss11 <isaiah.sinclair.0711@gmail.com>
2024-05-18 19:10:33 -0400

    feature/add-details-to-compare: Add Layout

--- frontend/src/pages/compare.jsx
+++ frontend/src/pages/compare.jsx
@@ -3,6 +3,9 @@ import { PageContainer } from "@/components/page-container";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import axios from "axios"
+import mockData from "./sampleCompare";
+import ProfileGeneral from "@/components/profileGeneral";
+
 
 export function ComparePage() {
     const [profiles, setProfiles] = useState("")
@@ -25,6 +28,13 @@ export function ComparePage() {
             <h1>Compare GitHub User Profiles</h1>
             <textarea id="compareInput" name="compareInput" rows="4" cols="10" value={profiles} onChange={(e) => setProfiles(e.target.value)} placeholder="Enter a list of users' GitHub profile URLs, with each URL on a separate line."></textarea>
             <Button onClick={(e) => handleSubmit(e)}>Compare Profiles</Button>
+            <h1>Results</h1>
+            {/* Container that wraps all the different inputs. */}
+            <ol className="list-decimal">
+                {mockData.profileScores?.map((profile, key) => {
+                    return <ProfileGeneral profile={profile} />
+                })}
+            </ol>
         </PageContainer>
     );
 }
