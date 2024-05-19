4cf434c0a808bfc5a40134d9d1ec306e596f4bef
Benjamin Sengupta <bsengupta@waveapps.com>
2024-05-19 04:53:59 -0400

    Format AIQuery.py

--- backend/src/routes/AIQuery.py
+++ backend/src/routes/AIQuery.py
@@ -7,51 +7,52 @@ jsonQueryExample = {
     "readability": 5,
     "bestProgrammingPractices": 7,
     "maintainability": 3,
-    "feedback": "Feedback is written here."
+    "feedback": "Feedback is written here.",
 }
+
 
 class AIQuery:
     def __init__(self) -> None:
         self.llm = self.getGemini()
-    
+
     # Sets up API key for Gemini, ready to query
-    def getGemini(self):   
-        self.GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+    def getGemini(self):
+        self.GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
         genai.configure(api_key=self.GOOGLE_API_KEY)
-        
-        return genai.GenerativeModel('gemini-pro')
-    
+
+        return genai.GenerativeModel("gemini-pro")
+
     def getNote(self, category, score):
         category = "impact"
         objectFormat = {
             "note": "A note is here.",
         }
-        
+
         query = f"Can you give a message about my score of {score} out of 10 for my overall '{category}' on GitHub in 8 words or less. It should not have any apostrophes. The response should be in the format:\n {objectFormat}"
-        
+
         note = self.llm.generate_content(query).text
-        
+
         print(note)
-        
+
         note = self.extract_json(note)
-        
-        return note[0]['note']
-        
+
+        return note[0]["note"]
+
     # Obtains a JSON response on a variety of grades from Gemini
     def getFeedback(self, stringifiedFiles):
         query = f"""Score the following code from different files on code readability with an integer, with the maximum value being 10. Then score the following code on best programming practices for the given programming language (such as object-oriented programming if the programming language used is Java, Kotlin, or another OOP language) with an integer, with the maximum value being 10. Than score the following code on maintainability with an integer, with the maximum value being 10. Also provide some general feedback on the code itself in a 'feedback' object -- it should be a maximum of 25 words.  Return it as a JSON object exactly as in this example:\n {jsonQueryExample}\nThe code to score is:\n {stringifiedFiles}"""
 
         feedback = self.llm.generate_content(query)
         feedback = feedback.text
-        
+
         feedback = self.extract_json(feedback)
 
         return feedback[0]
-    
+
     # Reference for JSON fix: https://learnwithhasan.com/consistent-json-gemini-python/
     def extract_json(self, text_response):
         # This pattern matches a string that starts with '{' and ends with '}'
-        pattern = r'\{[^{}]*\}'
+        pattern = r"\{[^{}]*\}"
         text = text_response.replace("'", '"')
 
         matches = re.finditer(pattern, text)
@@ -60,7 +61,7 @@ class AIQuery:
         for match in matches:
             json_str = match.group(0)
             try:
-                
+
                 print(json_str)
                 # Validate if the extracted string is valid JSON
                 json_obj = json.loads(json_str)
@@ -86,30 +87,31 @@ class AIQuery:
         start, end = span
         nest_count = 0
         for i in range(start, len(text)):
-            if text[i] == '{':
+            if text[i] == "{":
                 nest_count += 1
-            elif text[i] == '}':
+            elif text[i] == "}":
                 nest_count -= 1
                 if nest_count == 0:
-                    return text[start:i+1]
+                    return text[start : i + 1]
         return text[start:end]
-    
+
     # Creates string of files to be used in the prompt, given the file paths.
     def getStringifiedFiles(self, fileContent):
         stringifiedFiles = ""
-    
+
         for language in fileContent:
-            files = fileContent[language]['files']
-            
+            files = fileContent[language]["files"]
+
             for file in files:
-                stringifiedFiles += file['content']
+                stringifiedFiles += file["content"]
                 stringifiedFiles += "\n\n"
-            
+
         return stringifiedFiles
-    
+
     # Takes a list of file paths and obtains a score and feedback from Gemini on a series of criteria
     def gradeFiles(self, filePaths):
         stringifiedFiles = self.getStringifiedFiles(filePaths)
         feedback = self.getFeedback(stringifiedFiles, filePaths[0])
-        
+
         return feedback
+
